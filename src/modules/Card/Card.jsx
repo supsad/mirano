526a17c6f0d050e3b15f6518016133e290606b66
Dmitry Void <supsad@yahoo.com>
2024-07-05 18:15:37 +0700

    Convert Card module

--- src/modules/Card/Card.jsx
+++ src/modules/Card/Card.jsx
@@ -1,18 +1,17 @@
-import './card.scss';
-import classNames from 'classnames';
+import styles from './Card.module.scss';
 
-export const Card = ({ className, img, title, dateDelivery, price}) => (
-  <article className={ classNames(className, 'card') }>
+export const Card = ({ img, title, dateDelivery, price }) => (
+  <article className={ styles.card }>
     <img
-      className="card__image"
+      className={ styles.image }
       src={ img }
       alt={ title }
     />
-    <div className="card__content">
-      <h3 className="card__title">{ title }</h3>
-      <div className="card__footer">
-        <p className="card__date-delivery">{ dateDelivery }</p>
-        <button className="card__button">{ price }&nbsp;₽</button>
+    <div className={ styles.content }>
+      <h3 className={ styles.title }>{ title }</h3>
+      <div className={ styles.footer }>
+        <p className={ styles.dateDelivery }>{ dateDelivery }</p>
+        <button className={ styles.button }>{ price }&nbsp;₽</button>
       </div>
     </div>
   </article>
