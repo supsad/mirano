cc2a1d4a4bcb391ae3f6247732443ceeb0db29cf
Dmitry Void <supsad@yahoo.com>
2024-07-05 18:16:08 +0700

    Convert Cart module

--- src/modules/Cart/Cart.jsx
+++ src/modules/Cart/Cart.jsx
@@ -1,62 +1,50 @@
-import './cart.scss';
+import styles from './Cart.module.scss';
+import classNames from 'classnames';
+import { CartItem } from '../CartItem/CartItem.jsx';
 
-export const Cart = () => (
-  <section className="cart cart_open">
-    <div className="cart__container">
-      <div className="cart__header">
-        <h3 className="cart__title">Ваш заказ</h3>
+export const Cart = () => {
+  const tempArr = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
-        <button className="cart__close">
-          <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="5" y="5.70715" width="1" height="25" transform="rotate(-45 5 5.70715)" fill="#D17D2F" />
-            <rect x="22.6777" y="5" width="1" height="25" transform="rotate(45 22.6777 5)" fill="#D17D2F" />
-          </svg>
-        </button>
+  const isOpen = false;
+
+  if (!isOpen) return null;
+
+  return (
+    <section className={ classNames(styles.cart, styles.cart_open) }>
+      <div className={ styles.container }>
+        <div className={ styles.header }>
+          <h3 className={ styles.title }>Ваш заказ</h3>
+
+          <button>
+            <svg
+              width="28"
+              height="28"
+              viewBox="0 0 28 28"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <rect x="5" y="5.70715" width="1" height="25" transform="rotate(-45 5 5.70715)" fill="#D17D2F" />
+              <rect x="22.6777" y="5" width="1" height="25" transform="rotate(45 22.6777 5)" fill="#D17D2F" />
+            </svg>
+          </button>
+        </div>
+
+        <p className={ styles['date-delivery'] }>сегодня в 14:00</p>
+
+        <ul className={ styles.list }>
+          {
+            // TODO Replace tempArr
+            tempArr.map(item => (
+              <CartItem key={ item.id } />
+            ))
+          }
+        </ul>
+
+        <div className={ styles.footer }>
+          <button className={ styles['order-btn'] }>Оформить</button>
+          {/* FIXME: ? styles.priceTotal */ }
+          <p className={ styles.price }>0&nbsp;₽</p>
+        </div>
       </div>
-
-      <p className="cart__date-delivery">сегодня в 14:00</p>
-
-      <ul className="cart__list">
-        <li className="cart__item">
-          <img className="cart__img" src="https://dull-rose-pawpaw.glitch.me/img/39.jpg"
-               alt="Букет из роз Grand Avalanche (101 шт)" />
-          <h4 className="cart__item-title">Букет из роз Grand Avalanche (101 шт)</h4>
-          <div className="cart__counter">
-            <button className="cart__counter-btn">-</button>
-            <input className="cart__counter-input" type="number" max="99" min="0" value="1" />
-            <button className="cart__counter-btn">+</button>
-          </div>
-          <p className="cart__price">14800&nbsp;₽</p>
-        </li>
-        <li className="cart__item">
-          <img className="cart__img" src="https://dull-rose-pawpaw.glitch.me/img/38.jpg"
-               alt="Букет из тюльпан Dolche vita (51 шт)" />
-          <h4 className="cart__item-title">Букет из тюльпан Dolche vita (51 шт)</h4>
-          <div className="cart__counter">
-            <button className="cart__counter-btn">-</button>
-            <input className="cart__counter-input" type="number" max="99" min="0" value="2" />
-            <button className="cart__counter-btn">+</button>
-          </div>
-          <p className="cart__price">13400&nbsp;₽</p>
-        </li>
-        <li className="cart__item">
-          <img className="cart__img" src="https://dull-rose-pawpaw.glitch.me/img/41.jpg"
-               alt="Букет из роз Grand Mirabel (101 шт)" />
-          <h4 className="cart__item-title">Букет из роз Grand Mirabel (101 шт)</h4>
-          <div className="cart__counter">
-            <button className="cart__counter-btn">-
-            </button>
-            <input className="cart__counter-input" type="number" max="99" min="0" value="3" />
-            <button className="cart__counter-btn">+</button>
-          </div>
-          <p className="cart__price">63900&nbsp;₽</p>
-        </li>
-      </ul>
-
-      <div className="cart__footer">
-        <button className="cart__order-btn">Оформить</button>
-        <p className="cart__price cart__price_total">0&nbsp;₽</p>
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  )
+};
