46836995fd0d69050ba4a4abc536aa8646b4f337
Dmitry Void <supsad@yahoo.com>
2024-07-05 18:16:48 +0700

    Convert Header module

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -1,13 +1,23 @@
-import './header.scss';
+import styles from './Header.module.scss';
+import classNames from 'classnames';
 
-export const Header = () => (
-  <header className="header">
-    <div className="container header__container">
-      <form className="header__form" action="#">
-        <input className="header__input" type="search" name="search" placeholder="Букет из роз" />
+export const Header = ({ className }) => (
+  <header className={ styles.header }>
+    <div className={ classNames(className, styles.container) }>
+      <form className={ styles.form } action="#">
+        <input
+          className={ styles.input }
+          type="search"
+          name="search"
+          placeholder="Букет из роз" />
 
-        <button className="header__search-button" aria-label="начать поиск">
-          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <button className={ styles['search-button'] } aria-label="начать поиск">
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
             <path
               d="M13.3333 4.16663C13.3333 4.78496 13.9442 5.70829 14.5625 6.48329C15.3575 7.48329 16.3075 8.35579 17.3967 9.02163C18.2133 9.52079 19.2033 9.99996 20 9.99996M20 9.99996C19.2033 9.99996 18.2125 10.4791 17.3967 10.9783C16.3075 11.645 15.3575 12.5175 14.5625 13.5158C13.9442 14.2916 13.3333 15.2166 13.3333 15.8333M20 9.99996H4.76837e-07"
               stroke="white" />
@@ -15,9 +25,9 @@ export const Header = () => (
         </button>
       </form>
 
-      <img className="header__logo" src="/img/logo.svg" alt="Логотип Mirano Flower Boutique" />
+      <img className={ styles.logo } src="/img/logo.svg" alt="Логотип Mirano Flower Boutique" />
 
-      <button className="header__cart-button">0</button>
+      <button className={ styles['cart-button'] }>0</button>
     </div>
   </header>
 );
