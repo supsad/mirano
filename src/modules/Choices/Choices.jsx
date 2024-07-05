cd755541227a385fc1859a65f59d7ea6ae2c704f
Dmitry Void <supsad@yahoo.com>
2024-07-05 18:16:19 +0700

    Convert Choices module

--- src/modules/Choices/Choices.jsx
+++ src/modules/Choices/Choices.jsx
@@ -1,28 +1,49 @@
-import './choices.scss';
+import styles from './Choices.module.scss';
+import classNames from 'classnames';
 
-export const Choices = () => (
-  <div className="filter__choices filter__choices_type choices">
-    <button className="filter__select choices__btn" type="button">Тип товара</button>
+const TYPES = ['price', 'type'];
 
-    <div
-      className="choices__box filter__choices-box filter__choices-box_type">
-      <ul className="filter__type-list">
-        <li className="filter__type-item">
-          <button className="filter__type-button" type="button">Монобукеты</button>
-        </li>
-        <li className="filter__type-item">
-          <button className="filter__type-button" type="button">Авторские букеты</button>
-        </li>
-        <li className="filter__type-item">
-          <button className="filter__type-button" type="button">Цветы в коробке</button>
-        </li>
-        <li className="filter__type-item">
-          <button className="filter__type-button" type="button">Цветы в корзине</button>
-        </li>
-        <li className="filter__type-item">
-          <button className="filter__type-button" type="button">Букеты из сухоцветов</button>
-        </li>
-      </ul>
+export const Choices = ({ type }) => {
+  const isOpen = false;
+
+  if (!isOpen || type === undefined || type === null || type === '') return null;
+
+  return (
+    <div className={ classNames(styles.choices, styles.choices_open) }>
+      {
+        type.toLowerCase() === TYPES[0] ? (
+          <fieldset className={ styles.price }>
+            <input
+              className={ styles['input-price'] }
+              type="text"
+              name="minPrice"
+              placeholder="от" />
+            <input
+              className={ styles['input-price'] }
+              type="text"
+              name="maxPrice"
+              placeholder="до" />
+          </fieldset>
+        ) : (
+          <ul>
+            <li className={ styles['type-item'] }>
+              <button className={ styles['type-button'] } type="button">Монобукеты</button>
+            </li>
+            <li className={ styles['type-item'] }>
+              <button className={ styles['type-button'] } type="button">Авторские букеты</button>
+            </li>
+            <li className={ styles['type-item'] }>
+              <button className={ styles['type-button'] } type="button">Цветы в коробке</button>
+            </li>
+            <li className={ styles['type-item'] }>
+              <button className={ styles['type-button'] } type="button">Цветы в корзине</button>
+            </li>
+            <li className={ styles['type-item'] }>
+              <button className={ styles['type-button'] } type="button">Букеты из сухоцветов</button>
+            </li>
+          </ul>
+        )
+      }
     </div>
-  </div>
-);
+  )
+};
