862e07807046da7580a41d8f49cb47322a2701d4
Dmitry Void <supsad@yahoo.com>
2024-07-09 12:44:46 +0700

    add alias in code
    Alias doesn't work without a config in JetBrains WS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@scss', replacement: fileURLToPath(new URL('./src/scss', import.meta.url)) },
+    ],
+  },
 })
