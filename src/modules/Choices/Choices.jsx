5ee7ae4f64f9fbc96d51bb8a17f3a9c4e54e8058
Dmitry Void <supsad@yahoo.com>
2024-07-10 22:01:05 +0700

    Add closing filters by index
    When you open any other filter, not counting the currently open one, the open one closes

--- src/modules/Choices/Choices.jsx
+++ src/modules/Choices/Choices.jsx
@@ -1,26 +1,19 @@
 import styles from './Choices.module.scss';
 import classNames from 'classnames';
-import { useState } from 'react';
 
-export const Choices = ({ children, buttonLabel, className }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleToggle = () => setIsOpen(oldIsOpen => !oldIsOpen);
-
-  return (
-    <div className={ styles.choices }>
-      <button
-        className={ styles.select }
-        type="button"
-        onClick={ handleToggle }>
-        { buttonLabel }
-      </button>
-      {
-        isOpen &&
-        <div className={ classNames(styles.container, className) }>
-          { children }
-        </div>
-      }
-    </div>
-  )
-};
+export const Choices = ({ children, buttonLabel, className, isOpen, handleToggle }) => (
+  <div className={ styles.choices }>
+    <button
+      className={ styles.select }
+      type="button"
+      onClick={ handleToggle }>
+      { buttonLabel }
+    </button>
+    {
+      isOpen &&
+      <div className={ classNames(styles.container, className) }>
+        { children }
+      </div>
+    }
+  </div>
+);
--- src/modules/Filter/Filter.jsx
+++ src/modules/Filter/Filter.jsx
@@ -1,89 +1,104 @@
 import styles from './Filter.module.scss';
 import { Choices } from '@modules/Choices/Choices.jsx';
+import { useState } from 'react';
 import classNames from 'classnames';
 
-export const Filter = ({ titleClass, containerClass }) => (
-  <section>
-    <h2 className={ titleClass }></h2>
-    <div className={ containerClass }>
-      <form className={ styles.form }>
-        <fieldset className={ styles.group }>
-          <input
-            className={ styles.radio }
-            type="radio"
-            name="type"
-            value="bouquets"
-            id="flower"
-            defaultChecked />
-          <label
-            className={ classNames(styles.label, styles.label_flower) }
-            htmlFor="flower">
-            Цветы
-          </label>
+export const Filter = ({ titleClass, containerClass }) => {
+  const [openChoices, setOpenChoices] = useState(null);
 
-          <input
-            className={ styles.radio }
-            type="radio"
-            name="type"
-            value="toys"
-            id="toys" />
-          <label
-            className={ classNames(styles.label, styles.label_toys) }
-            htmlFor="toys">
-            Игрушки
-          </label>
+  const handleChoicesToggle = (index) => {
+    setOpenChoices(openChoices === index ? null : index);
+  }
 
-          <input
-            className={ styles.radio }
-            type="radio"
-            name="type"
-            value="postcards"
-            id="postcard" />
-          <label
-            className={ classNames(styles.label, styles.label_postcard) }
-            htmlFor="postcard">
-            Открытки
-          </label>
-        </fieldset>
+  return (
+    <section>
+      <h2 className={ titleClass }></h2>
+      <div className={ containerClass }>
+        <form className={ styles.form }>
+          <fieldset className={ styles.group }>
+            <input
+              className={ styles.radio }
+              type="radio"
+              name="type"
+              value="bouquets"
+              id="flower"
+              defaultChecked />
+            <label
+              className={ classNames(styles.label, styles.label_flower) }
+              htmlFor="flower">
+              Цветы
+            </label>
 
-        <fieldset className={ classNames(styles.group, styles.group_choices) }>
-          <Choices buttonLabel={ 'Цена' }>
-            <fieldset className={ styles.price }>
-              <input
-                className={ styles['input-price'] }
-                type="text"
-                name="minPrice"
-                placeholder="от" />
-              <input
-                className={ styles['input-price'] }
-                type="text"
-                name="maxPrice"
-                placeholder="до" />
-            </fieldset>
-          </Choices>
+            <input
+              className={ styles.radio }
+              type="radio"
+              name="type"
+              value="toys"
+              id="toys" />
+            <label
+              className={ classNames(styles.label, styles.label_toys) }
+              htmlFor="toys">
+              Игрушки
+            </label>
+
+            <input
+              className={ styles.radio }
+              type="radio"
+              name="type"
+              value="postcards"
+              id="postcard" />
+            <label
+              className={ classNames(styles.label, styles.label_postcard) }
+              htmlFor="postcard">
+              Открытки
+            </label>
+          </fieldset>
+
+          <fieldset className={ classNames(styles.group, styles.group_choices) }>
+            <Choices
+              buttonLabel={ 'Цена' }
+              isOpen={ openChoices === 0 }
+              handleToggle={ () => handleChoicesToggle(0) }>
+              <fieldset className={ styles.price }>
+                <input
+                  className={ styles['input-price'] }
+                  type="text"
+                  name="minPrice"
+                  placeholder="от" />
+                <input
+                  className={ styles['input-price'] }
+                  type="text"
+                  name="maxPrice"
+                  placeholder="до" />
+              </fieldset>
+            </Choices>
 
 
-          <Choices buttonLabel={ 'Тип товара' }>
-            <ul>
-              <li className={ styles['type-item'] }>
-                <button className={ styles['type-button'] } type="button">Монобукеты</button>
-              </li>
-              <li className={ styles['type-item'] }>
-                <button className={ styles['type-button'] } type="button">Авторские букеты</button>
-              </li>
-              <li className={ styles['type-item'] }>
-                <button className={ styles['type-button'] } type="button">Цветы в коробке</button>
-              </li>
-              <li className={ styles['type-item'] }>
-                <button className={ styles['type-button'] } type="button">Цветы в корзине</button>
-              </li>
-              <li className={ styles['type-item'] }>
-                <button className={ styles['type-button'] } type="button">Букеты из сухоцветов</button>
-              </li>
-            </ul>
-          </Choices>
-        </fieldset>
-      </form>
-    </div>
-  </section>
-);
+            <Choices
+              buttonLabel={ 'Тип товара' }
+              isOpen={ openChoices === 1 }
+              handleToggle={ () => handleChoicesToggle(1) }>
+              <ul>
+                <li className={ styles['type-item'] }>
+                  <button className={ styles['type-button'] } type="button">Монобукеты</button>
+                </li>
+                <li className={ styles['type-item'] }>
+                  <button className={ styles['type-button'] } type="button">Авторские букеты</button>
+                </li>
+                <li className={ styles['type-item'] }>
+                  <button className={ styles['type-button'] } type="button">Цветы в коробке</button>
+                </li>
+                <li className={ styles['type-item'] }>
+                  <button className={ styles['type-button'] } type="button">Цветы в корзине</button>
+                </li>
+                <li className={ styles['type-item'] }>
+                  <button className={ styles['type-button'] } type="button">Букеты из сухоцветов</button>
+                </li>
+              </ul>
+            </Choices>
+          </fieldset>
+        </form>
+      </div>
+    </section>
+  );
+}
