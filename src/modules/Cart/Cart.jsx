6c2e278af61212c76c8c917aaa09f5ae2a6101a9
Dmitry Void <supsad@yahoo.com>
2024-07-11 15:15:29 +0700

    Remove style and positional errors in the cart
    * cart no longer falls out of the block
    * add a shadow for better UX when the cart starts to “hover” above the layout
    * scroll no longer overlaps cart items
    * size of the cart has been fixed, it no longer falls out of the browser window
    * adherence to the new style code

--- src/modules/Cart/Cart.jsx
+++ src/modules/Cart/Cart.jsx
@@ -1,10 +1,9 @@
 import styles from './Cart.module.scss';
-import classNames from 'classnames';
 import { CartItem } from '@modules/CartItem/CartItem.jsx';
 import { goodsArray } from '@/goodsArray.js';
 import { useDispatch, useSelector } from 'react-redux';
-import { toggleCart } from '@redux/cartSlice.js';
-import { openOrder } from '@redux/orderSlice.js';
+import { toggleCart } from '@store/reducers/cartSlice.js';
+import { openOrder } from '@store/reducers/orderSlice.js';
 
 export const Cart = () => {
   const dispatch = useDispatch();
@@ -16,18 +15,17 @@ export const Cart = () => {
   if (!isOpen) return null;
 
   return (
-    <section className={ classNames(styles.cart, styles.cart_open) }>
+    <section className={ styles.cart }>
       <div className={ styles.container }>
         <div className={ styles.header }>
           <h3 className={ styles.title }>Ваш заказ</h3>
 
           <button onClick={ handlerCartClose }>
-            <svg
-              width="28"
-              height="28"
-              viewBox="0 0 28 28"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
+            <svg width="28"
+                 height="28"
+                 viewBox="0 0 28 28"
+                 fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
               <rect x="5" y="5.70715" width="1" height="25" transform="rotate(-45 5 5.70715)" fill="#D17D2F" />
               <rect x="22.6777" y="5" width="1" height="25" transform="rotate(45 22.6777 5)" fill="#D17D2F" />
             </svg>
@@ -45,9 +43,8 @@ export const Cart = () => {
         </ul>
 
         <div className={ styles.footer }>
-          <button
-            className={ styles['order-btn'] }
-            onClick={ handlerOrderOpen }>
+          <button className={ styles['order-btn'] }
+                  onClick={ handlerOrderOpen }>
             Оформить
           </button>
           <p className={ styles.price }>0&nbsp;₽</p>
