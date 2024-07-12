87515efd89bce5c6dd7cd2b78b6dd685ff33fa6f
Dmitry Void <supsad@yahoo.com>
2024-07-12 19:00:45 +0700

    Add replacing the price of the product card with text on hover

---
+++ src/hooks/useHover.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export default function useHover() {
+  const [hovering, setHovering] = useState(false);
+  const onHoverProps = {
+    onMouseEnter: () => setHovering(true),
+    onMouseLeave: () => setHovering(false),
+  };
+
+  return [hovering, onHoverProps];
+}
