1f7f940816d104b8a5a80a681c09b52c371cbf5d
Dmitry Void <supsad@yahoo.com>
2024-07-18 21:16:40 +0700

    Add a title when downloading products from the server

--- src/modules/Goods/Goods.jsx
+++ src/modules/Goods/Goods.jsx
@@ -24,15 +24,23 @@ export const Goods = ({ containerClass, titleClass }) => {
     dispatch(setGoodsTitleCoords(titleCoords.top));
   }, [dispatch])
 
+  let titleValue = null;
   let content = null;
 
   if (goodsStatus === 'loading') {
-    content = <Loader />
+    titleValue = 'Загрузка...';
+    content = <Loader />;
   } else if (goodsStatus === 'succeeded') {
-    content = <ul className={ styles.list }>
-      {
-        goods.length !== 0
-          ?
+    if (goods.length === 0) {
+      titleValue = 'Товары не найдены!';
+      content = <p className={ styles['no-product'] } style={ { lineHeight: '1.6em' } }>
+        По вашему запросу ничего не найдено!<br />
+        Попробуйте ввести другой!
+      </p>;
+    } else {
+      titleValue = goodsTitle.value;
+      content = <ul className={ styles.list }>
+        {
           goods.map(item => (
             <li key={ item.id }>
               <Card id={ item.id }
@@ -43,13 +51,9 @@ export const Goods = ({ containerClass, titleClass }) => {
               />
             </li>
           ))
-          :
-          <p className={ styles['no-product'] } style={{lineHeight: '1.6em'}}>
-            По вашему запросу ничего не найдено!<br />
-            Попробуйте ввести другой!
-          </p>
-      }
-    </ul>
+        }
+      </ul>;
+    }
   }
 
   if (error) {
@@ -60,9 +64,7 @@ export const Goods = ({ containerClass, titleClass }) => {
     <section className={ styles.goods }>
       <div className={ classNames(containerClass, styles.container) }>
         <div className={ styles.box } style={ { position: 'relative' } }>
-          <h2 className={ titleClass } ref={ goodsTitleRef }>
-            { goods.length !== 0 ? goodsTitle.value : 'Товары не найдены!' }
-          </h2>
+          <h2 className={ titleClass } ref={ goodsTitleRef }>{ titleValue }</h2>
 
           { content }
         </div>
